fb6f1dc5bfd47739cb1a61b17fa5e8cb3f34745b
Akankshya26 <119608124+Akankshya26@users.noreply.github.com>
2023-02-24 15:36:48 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -879,6 +879,7 @@
                                 documentation from beginning to end.
                             </p>
                             <p>hello this is merge conflict test-p1</p>
+                            <p>hello this is merge conflict test-p2</p>
                         </div>
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
